dad9d2ffdacb0862770124ad1e3a266f34a3d601
winniebinnie <ritthanupahp_s@cmu.ac.th>
2024-03-15 10:56:44 +0700

    Fixed PM25Plugin

--- PM25Plugin.cs
+++ PM25Plugin.cs
@@ -18,21 +18,22 @@ namespace DNWS
 
         public PM25Plugin()
         {
-            if (pm25reading == null) {
+            if (pm25reading == null)
+            {
                 RequestAqiCn();
             }
         }
 
         public void RequestAqiCn()
         {
-            HttpWebRequest http = (HttpWebRequest) WebRequest.Create("http://api.waqi.info/feed/shanghai/?token=demo");
-            HttpWebResponse response = (HttpWebResponse) http.GetResponse();
+            HttpWebRequest http = (HttpWebRequest)WebRequest.Create("http://api.waqi.info/feed/shanghai/?token=demo");
+            HttpWebResponse response = (HttpWebResponse)http.GetResponse();
             Stream stream = response.GetResponseStream();
             StreamReader sr = new StreamReader(stream);
             string content = sr.ReadToEnd();
             pm25reading = JsonSerializer.Deserialize<PM25Reading>(content);
         }
-        
+
         public void PreProcessing(HTTPRequest request)
         {
             throw new NotImplementedException();
@@ -40,26 +41,24 @@ namespace DNWS
 
         public HTTPResponse GetResponse(HTTPRequest request)
         {
-            if (pm25reading == null) {
+            if (pm25reading == null)
+            {
                 RequestAqiCn();
-            } else {
+            }
+            else
+            {
                 DateTime now = DateTime.Now;
                 TimeSpan diff = now.Subtract(pm25reading.data.time.iso.DateTime);
-                if (diff >= new TimeSpan(1, 0, 0)) {
+                if (diff >= new TimeSpan(1, 0, 0))
+                {
                     RequestAqiCn();
                 }
             }
 
-            HTTPResponse response = null;
-            StringBuilder sb = new StringBuilder();
-            sb.Append("<html><body>");
-            sb.Append("Location : " + pm25reading.data.city.name + "<br />");
-            sb.Append("Date/Time : " + pm25reading.data.time.iso + "<br />");
-            sb.Append("PM25 : " + pm25reading.data.iaqi.pm25.v + "<br />");
-            sb.Append("PM10 : " + pm25reading.data.iaqi.pm10.v + "<br />");
-            sb.Append("</body></html>");
-            response = new HTTPResponse(200);
-            response.body = Encoding.UTF8.GetBytes(sb.ToString());
+            HTTPResponse response = new HTTPResponse(200);
+            string JsonResponse = JsonSerializer.Serialize(pm25reading);
+            response.body = Encoding.UTF8.GetBytes(JsonResponse);
+            response.type = "application/json"; // Set content type to JSON
             return response;
         }
 
@@ -69,3 +68,4 @@ namespace DNWS
         }
     }
 }
+
